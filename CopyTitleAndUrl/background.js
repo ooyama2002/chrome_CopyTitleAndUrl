41547d2391736c236995da3f3911c92be97b06b4
ooyama2002 <ooyama2002@gmail.com>
2024-08-06 18:31:30 +0900

    cancel opera

--- CopyTitleAndUrl/background.js
+++ CopyTitleAndUrl/background.js
@@ -13,7 +13,6 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     }
 
     if (tab.url?.startsWith("chrome://")) return undefined;
-    if (tab.url?.startsWith("opera://")) return undefined;
 
     chrome.tabs.sendMessage(tab.id, buff);
 });
