8a00368b210af0626b38971d9b89bd25ed0d4d45
ooyama2002 <ooyama2002@gmail.com>
2024-08-06 11:54:44 +0900

    v1.1.2

--- CopyTitleAndUrl/background.js
+++ CopyTitleAndUrl/background.js
@@ -1,55 +1,37 @@
-{
-    chrome.runtime.onInstalled.addListener((details) => {
+// リスナーの登録
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    let buff = "";
 
+    if (info.menuItemId == "normal") {
+        buff = tab.title + "\r\n" + tab.url;
+    } else if (info.menuItemId == "markdown") {
+        buff = "[" + tab.title + "](" + tab.url + ")";
+    } else if (info.menuItemId == "pukiwiki") {
+        buff = "[[" + tab.title + ":" + tab.url + "]]";
+    } else if (info.menuItemId == "html") {
+        buff = "<a href=\"" + tab.url + "\">" + tab.title + "</a>";
+    }
+
+    if (tab.url?.startsWith("chrome://")) return undefined;
+
+    chrome.tabs.sendMessage(tab.id, buff);
+});
+
+// コンテキストメニューの登録
+chrome.runtime.onInstalled.addListener((details) => {
+
+    let ids = [
+        "normal",
+        "markdown",
+        "pukiwiki",
+        "html"
+    ];
+
+    for (let i = 0; i < ids.length; i++) {
         chrome.contextMenus.create({
-            type: "normal",
-            id: "normal",
-            title: "normal",
+            id: ids[i],
+            title: ids[i],
             contexts: ["all"],
         });
-
-        chrome.contextMenus.create({
-            type: "normal",
-            id: "markdown",
-            title: "markdown",
-            contexts: ["all"],
-        });
-
-        chrome.contextMenus.create({
-            type: "normal",
-            id: "pukiwiki",
-            title: "pukiwiki",
-            contexts: ["all"],
-        });
-
-        chrome.contextMenus.create({
-            type: "normal",
-            id: "html",
-            title: "html",
-            contexts: ["all"],
-        });
-
-    });
-
-    chrome.contextMenus.onClicked.addListener((info, tab) => {
-        let buff = "";
-
-        if (info.menuItemId == "normal") {
-            buff = tab.title + "\r\n" + tab.url;
-        } else if (info.menuItemId == "markdown") {
-            buff = "[" + tab.title + "](" + tab.url + ")";
-        } else if (info.menuItemId == "pukiwiki") {
-            buff = "[[" + tab.title + ":" + tab.url + "]]";
-        } else if (info.menuItemId == "html") {
-            buff = "<a href=\"" + tab.url + "\">" + tab.title + "</a>";
-        }
-
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            args: [buff],
-            function: (param) => {
-                navigator.clipboard.writeText(param);
-            },
-        });
-    });
-}
+    };
+});
